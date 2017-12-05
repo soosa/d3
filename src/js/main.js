6215ff30bf631c7b5fc8d70ca81a2cec796e6e00
Anett_Soos <anett.soos@city.ac.uk>
2017-12-05 17:05:18 +0000

    tidy up code

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,14 @@
 import scores from '../../data/scores.js';
 
-let update = d3.select('.chart')
-  .selectAll('div')                     // as there are no divs this is only data
-  .data(scores, function (d) {          // pair each number in the array with an empty div 
-    return d ? d.name : this.innerText; // check if name already exists
-  })
-  .style('color', 'blue'); 
-
-let enter = update.enter() // to access the enter selection, we'll use the .enter method on the update selection.
-  .append('div') // creating elements
-  .text((d) => d.name )
-  .style('color', 'green');  //only updated will be green
+d3.select('.chart')
+  .selectAll('div')
+  .data(scores)
+  .enter()
+    .append('div')
+    .text((d) => d.name )
+    .style('color', 'green');  //only updated will be green
 
 update.exit().remove();  // removes Walter as it is not in the data(update)
 
 update.merge(enter)
-  .style('width', d => d.score + 'px')
-  .style('height', '30px')
-  .style('border', '1px solid grey')
-  .style('background', 'orange')
-  .style('margin-top', '2px');
+  .style('width', d => d.score + 'px');
