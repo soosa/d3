c48b005d8834b02c28eaf28431755df835fedae2
Anett_Soos <anett.soos@city.ac.uk>
2017-12-06 13:30:27 +0000

    svg - <g> <rect> <text> elements

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,20 @@
 import scores from '../../data/scores.js';
 
-d3.select('.chart')
-  .append('svg')
-    .attr('width', 225)  // to overwrite default values
+let bar = d3.select('.chart')  // select the chart target div
+  .append('svg')               // appended an explicitly sized SVG  
+    .attr('width', 225)  
     .attr('height', 300)
-  .selectAll('div')
+  .selectAll('g')  // data join methodology to create a collection of G elements
   .data(scores)
   .enter()
-    .append('rect')
-    .attr('y', (d, i) => i * 33) // d -> data, i -> index
-    .style('width', d => d.score)
-    .classed('bars', true);
+    .append('g')
+    .attr('transform', (d, i) => 'translate( 10, ' + i * 35 + ')');
+
+bar.append('rect')  //append a rectangle and a text element to each one of the graphics containers
+  .style('width', d => d.score)
+  .text( d => d.name)
+  .classed('bars', true);
+
+bar.append('text')
+  .attr('y', 20)
+  .text( d => d.name);
