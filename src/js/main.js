01cba7d40e9fb25f59787ae53735326c52cdd8ae
Anett_Soos <anett.soos@city.ac.uk>
2017-12-11 09:40:48 +0000

    chart axes

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,39 @@
 
-let margin = { top: 25, right: 25, bottom: 25, left: 25 };
-// created our margin object with Top, Right, Bottom, and Left properties
+let margin = { top: 25, right: 25, bottom: 25, left: 45 };
 let width = 425 - margin.left - margin.right;
 let height = 625 - margin.top - margin.bottom;
-// then created width and height variables that take those margin properties into account
 
-let svg =  d3.select('.chart') // constructed our svg element and set its width and height to the full numeric values by adding the margin properties back in
+let svg =  d3.select('.chart')
   .append('svg')
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
   .append('g')
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
-  // then created a graphics container and moved it according to the left and top margins,
-  // so that all graphics created after that were already starting from the proper point.
 
 svg.append('rect')
-  .attr('width', width / 2)
+  .attr('width', width)
   .attr('height', height)
   .style('fill', 'lightblue')
-  .style('stroke', 'green');
-  
-  svg.append('rect')
-  .attr('x', width / 2 + 3)
-  .attr('width', width / 2)
-  .attr('height', height)
-  .style('fill', 'lightpink')
-  .style('stroke', 'red');
+  .style('stroke', 'grey');
+
+let yScale = d3.scaleLinear()
+  .domain([0, 1])
+  .range([height, 0]);  // y dimension runs from top to bottom in svg
+
+let yAxis = d3.axisLeft(yScale); //.ticks(5, '%') etc. to customise
+svg.call(yAxis);
+
+let xScale = d3.scaleTime()
+  .domain([new Date(2017, 9, 1), new Date(2017, 12, 1) ])
+  .range([0,width]);
+
+let xAxis = d3.axisBottom(xScale)
+    .ticks(5)
+    .tickSizeInner(10)
+    .tickSizeOuter(20)
+    .tickPadding(5);
+
+svg
+  .append('g')
+    .attr('transform', `translate(0, ${height})`)
+  .call(xAxis);
